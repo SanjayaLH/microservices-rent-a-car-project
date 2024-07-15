9f374e503e598c617a9196db5f7d4b5bb5d65794
SanjayaLH <sanjayahalwala@gmail.com>
2024-07-16 01:29:38 +0530

    add  config-local microservice in video[005]

---
+++ config-local/src/test/java/com/sanjaya/rentcloud/config_local/ConfigLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjaya.rentcloud.config_local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
